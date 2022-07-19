52e0ac8ecd6c366f9b934a9195cfb4b407b75118
kyeongan <tjsruddks12@gmail.com>
2022-07-19 15:18:53 +0900

    extract to csv

--- market_11/settings.py
+++ market_11/settings.py
@@ -3,7 +3,7 @@ BOT_NAME = 'market_11'
 
 SPIDER_MODULES = ['market_11.spiders']
 NEWSPIDER_MODULE = 'market_11.spiders'
-
+# DOWNLOAD_DELAY = 1
 ROBOTSTXT_OBEY = False
+FEED_EXPORT_ENCODING = 'cp949'#or utf-8 한글깨질경우
 
-DOWNLOAD_DELAY = 1
--- market_11/spiders/spider_11.py
+++ market_11/spiders/spider_11.py
@@ -3,17 +3,17 @@ from market_11.items import Market11Item
 
 class Spider11Spider(scrapy.Spider):
     name = 'spider_11'
-    allowed_domains = ['www.11st.co.kr']
+    allowed_domains = ['www.11st.co.kr/browsing/BestSeller.tmall?method=getBestSellerMain']
     start_urls = ['http://www.11st.co.kr/browsing/BestSeller.tmall?method=getBestSellerMain']
+                
 
     def parse(self, response):
-        # pass
-
-        titles = response.css('div#bestPrdList div.viewtype.catal_ty ul li div a div.pname p::text').getall()
-
-        for t in titles:         # t에 상품명 데이터 저장
-            item = Market11Item()    # (2) 객체 = <Item 이름>()
-
-        item['title'] = t    # (3-a) 객체['필드 이름'] = 추출한 데이터
-        yield item           # (3-b) yield 객체
+                titles = response.css('div#bestPrdList div.viewtype.catal_ty ul li div a div.pname p::text').getall()
+                for t in titles:         # t에 상품명 데이터 저장
+                    item = Market11Item()    # (2) 객체 = <Item 이름>()
+                    item['title'] = t
+                    print(item)
+                
+                    # (3-a) 객체['필드 이름'] = 추출한 데이터
+                    yield item           # (3-b) yield 객체
 
