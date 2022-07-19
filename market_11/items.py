dd90317083bb3c4a417e2c076d5b54a497ad3bca
kyeongan <tjsruddks12@gmail.com>
2022-07-19 14:03:17 +0900

    first commit

---
+++ market_11/items.py
@@ -0,0 +1,7 @@
+import scrapy
+
+class Market11Item(scrapy.Item):
+
+    title = scrapy.Field()
+
+    pass
---
+++ market_11/settings.py
@@ -0,0 +1,9 @@
+
+BOT_NAME = 'market_11'
+
+SPIDER_MODULES = ['market_11.spiders']
+NEWSPIDER_MODULE = 'market_11.spiders'
+
+ROBOTSTXT_OBEY = False
+
+DOWNLOAD_DELAY = 1
